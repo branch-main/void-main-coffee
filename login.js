266aed9f675a139e4ad4ab6524d670073fa012bc
branch-main <alan.roncal@tecsup.edu.pe>
2024-07-12 09:14:02 -0500

    Add files via upload

---
+++ login.js
@@ -0,0 +1,49 @@
+const form = document.querySelector("form");
+const eField = form.querySelector(".email");
+const eInput = eField.querySelector("input");
+const pField = form.querySelector(".password");
+const pInput = pField.querySelector("input");
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+
+    (eInput.value == "") ? eField.classList.add("shake", "error") : checkEmail();
+    (pInput.value == "") ? pField.classList.add("shake", "error") : checkPass();
+
+    setTimeout(() => {
+        eField.classList.remove("shake");
+        pField.classList.remove("shake");
+    }, 500);
+
+    eInput.onkeyup = () => { checkEmail(); }
+    pInput.onkeyup = () => { checkPass(); }
+
+    function checkEmail() {
+        let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+        if (!eInput.value.match(pattern)) {
+            eField.classList.add("error");
+            eField.classList.remove("valid");
+            let errorTxt = eField.querySelector(".error-txt");
+
+            (eInput.value != "") ? errorTxt.innerText = "Ingresa un correo electrónico válido" : errorTxt.innerText = "El correo electrónico no puede estar en blanco";
+        } else {
+            eField.classList.remove("error");
+            eField.classList.add("valid");
+        }
+    }
+
+    function checkPass() {
+        if (pInput.value == "") {
+            pField.classList.add("error");
+            pField.classList.remove("valid");
+        } else {
+            pField.classList.remove("error");
+            pField.classList.add("valid");
+        }
+    }
+
+    if (!eField.classList.contains("error") && !pField.classList.contains("error")) {
+        // Aquí redirige a la página de destino cuando los campos están validados
+        window.location.href = "index.html";
+    }
+}
